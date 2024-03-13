d979bdd0d22a781ca998f0112f904182f1561768
Edward4762 <123575108+Edward4762@users.noreply.github.com>
2024-03-13 21:54:45 +0530

    Text Color Update

--- my-app/src/components/Framecomponent.js
+++ my-app/src/components/Framecomponent.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './Framecomponent.css';
-const brailleText1 = "Lorem Ipsum Is A Good Way To Type";
-const brailleText2 = "Lorem Is A Good Way To Type Vision";
-const brailleText3 = "Vision";
+const brailleText1 = "Navigation Through Dynamic Bluetooth Host Changing";
+const brailleText2 = "Vision revolutionizes navigation for the visually impaired by seamlessly adapting to changing environments. Its Bluetooth host-changing capabilities empower users with dynamic connectivity, ensuring a continuous navigation experience.";
+const brailleText3 = "Spatial Audio Assistive Technology";
+const brailleText4 = "revolutionary way of perceiving and navigating your surroundings through immersive sound, opening new dimensions for the visually impaired. Elevate your sensory experience with Vision– where spatial awareness meets innovation";
 
-const BrailleToEnglishConverter = ({ brailleText }) => {
+const BrailleToEnglishConverter = ({ brailleText,applyBlueColor }) => {
   const [displayBraille, setDisplayBraille] = useState('');
 
   useEffect(() => {
@@ -16,10 +17,72 @@ const BrailleToEnglishConverter = ({ brailleText }) => {
         'Bluetooth': '⠃⠇⠥⠑⠞⠕⠕⠞⠓',
         'Host': '⠓⠕⠎⠞',
         'Changing': '⠉⠓⠁⠝⠛⠊⠝⠛',
+        'changing': '⠉⠓⠁⠝⠛⠊⠝⠛',
         'Vision': '⠧⠊⠎⠊⠕⠝',
-        'Type': '⠞⠽⠏⠑',
-        'Is': '⠁⠽',
+        'revolutionizes': '⠗⠑⠧⠕⠇⠥⠞⠊⠕⠝⠊⠵⠑⠎',
+        'navigation': '⠝⠁⠧⠊⠛⠁⠞⠊⠕⠝',
+        'for': '⠋⠕⠗',
+        'the': '⠞⠓⠑',
+        'visually': '⠧⠊⠎⠥⠁⠇⠇⠽',
+        'impaired': '⠊⠍⠏⠁⠊⠗⠑⠙',
+        'by': '⠃⠽',
+        'seamlessly': '⠎⠑⠁⠍⠇⠑⠎⠎⠇⠽',
+        'adapting': '⠁⠙⠁⠏⠞⠊⠝⠛',
+        'to': '⠞⠕',
+        'chaging': '⠉⠓⠁⠝⠛⠊⠝⠛',
+        'environments.': '⠑⠝⠧⠊⠗⠕⠝⠍⠑⠝⠞⠎',
+        'environments': '⠑⠝⠧⠊⠗⠕⠝⠍⠑⠝⠞⠎',
+        'Its': '⠊⠞⠎',
+        'bluetooth': '⠃⠇⠥⠑⠞⠕⠕⠞⠓',
+        'host-changing': '⠓⠕⠎⠞⠤⠉⠓⠁⠝⠛⠊⠝⠛',
+        'capabilities': '⠉⠁⠏⠁⠃⠊⠇⠊⠞⠊⠑⠎',
+        'empower': '⠑⠍⠏⠕⠺⠑⠗',
+        'users': '⠥⠎⠑⠗⠎⠺⠊⠞⠓',
+        'dynamic': '⠙⠽⠝⠁⠍⠊⠉',
+        'connectivity': '⠉⠕⠝⠝⠑⠉⠞⠊⠧⠊⠞⠽',
+        'connectivity,': '⠉⠕⠝⠝⠑⠉⠞⠊⠧⠊⠞⠽',
+        'ensuring': '⠑⠝⠎⠥⠗⠊⠝⠛',
+        'a': '⠁',
+        'continuous': '⠉⠕⠝⠞⠊⠝⠥⠕⠥⠎',
+        'navigation': '⠝⠁⠧⠊⠛⠁⠞⠊⠕⠝',
+        'experience': '⠑⠭⠏⠑⠗⠊⠑⠝⠉⠑',
+        'Spatial': '⠎⠏⠁⠞⠊⠕⠁⠇',
+        'Audio': '⠁⠥⠙⠊⠕',
+        'Assistive': '⠁⠎⠎⠊⠎⠞⠊⠧⠑',
+        'Technology': '⠞⠑⠉⠓⠝⠕⠇⠕⠛⠽',
+        'revolutionary': '⠗⠑⠧⠕⠇⠥⠞⠊⠕⠝⠁⠗⠽',
+        'way': '⠺⠁⠽',
+        'of': '⠕⠋',
+        'perceiving': '⠏⠑⠗⠉⠑⠊⠧⠊⠝⠛',
+        'and': '⠁⠝⠙',
+        'navigating': '⠝⠁⠧⠊⠛⠁⠞⠊⠝⠛',
+        'your': '⠽⠕⠥⠗',
+        'surroundings': '⠎⠥⠗⠗⠕⠥⠝⠙⠊⠝⠛⠎',
+        'through': '⠞⠓⠗⠕⠥⠛⠓',
+        'immersive': '⠞⠓⠗⠕⠥⠛⠓',
+        'sound': '⠊⠍⠍⠑⠗⠎⠊⠧⠑',
+        'sound,': '⠊⠍⠍⠑⠗⠎⠊⠧⠑',
+        'opening': '⠎⠕⠥⠝⠙',
+        'new': '⠋⠕⠗',
+        'dimensions': '⠞⠓⠑',
+        'for': '⠧⠊⠎⠥⠁⠇⠇⠽',
+        'the': '⠊⠍⠏⠁⠊⠗⠑⠙',
+        'visually': '⠑⠭⠏⠑⠗⠊⠑⠝⠉⠑',
+        'impaired': '⠺⠊⠞⠓',
+        'impaired.': '⠺⠊⠞⠓',
+        'Elevate': '⠑⠇⠑⠧⠁⠞⠑',
+        'sensory': '⠎⠑⠝⠎⠕⠗⠽',
+        'experience': '⠑⠭⠏⠑⠗⠊⠑⠝⠉⠑',
+        'experience.': '⠑⠭⠏⠑⠗⠊⠑⠝⠉⠑',
+        'with': '⠺⠊⠞⠓',
+        'Vision–': '⠠⠧⠊⠎⠊⠕⠝–',
+        'where': '⠺⠓⠑⠗⠑',
+        'spatial': '⠎⠏⠁⠞⠊⠕⠁⠇',
+        'awareness': '⠁⠺⠁⠗⠑⠝⠑⠎⠎',
+        'meets': '⠍⠑⠑⠞⠎',
+        'innovation': '⠊⠝⠝⠕⠧⠁⠞⠊⠕⠝'
       };
+      
       
       return englishMapping[brailleWord] || brailleWord;
     };
@@ -41,7 +104,14 @@ const BrailleToEnglishConverter = ({ brailleText }) => {
 
   return (
     <div>
-      <h1 className="text-size3 text-col r-margin-bottom">{displayBraille}</h1>
+      <h1 className="text-col text-align r-margin-bottom font-fam">
+        {displayBraille.split(' ').map((word, index) => {
+          if (applyBlueColor && (word.toLowerCase() === 'bluetooth' || word==='⠃⠇⠥⠑⠞⠕⠕⠞⠓')) {
+            return <span key={index} className="bluetoothBlue">{word + ' '}</span>;
+          }
+          return <React.Fragment key={index}>{word} </React.Fragment>;
+        })}
+      </h1>
     </div>
   );
 };
@@ -49,10 +119,19 @@ const BrailleToEnglishConverter = ({ brailleText }) => {
 const FrameComponent = () => {
 
   return (
-    <div className="flex padding-top pad-left">
-      <BrailleToEnglishConverter key={1} brailleText={brailleText3} />
-      <BrailleToEnglishConverter key={2} brailleText={brailleText1} />
-      <BrailleToEnglishConverter key={3} brailleText={brailleText2} />
+    <div className="flex padding-top pad-left margin">
+      <div className='text-size3'>
+        <BrailleToEnglishConverter key={1} brailleText={brailleText1} applyBlueColor={true} />
+      </div>
+      <div className='text-size2'>
+        <BrailleToEnglishConverter key={2} brailleText={brailleText2} applyBlueColor={false} />
+      </div>
+      <div className='text-size3'>
+        <BrailleToEnglishConverter key={1} brailleText={brailleText3} applyBlueColor={false}/>
+      </div>
+      <div className='text-size2'>
+        <BrailleToEnglishConverter key={2} brailleText={brailleText4} applyBlueColor={false}/>
+      </div>
     </div>
   );
 };
